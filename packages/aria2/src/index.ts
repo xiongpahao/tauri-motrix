9a7680cafb8f44eade7163000657d2de759103ea
Taoister39 <Qiacrkeng@gmail.com>
2025-04-09 15:06:06 +0800

    chore: import

--- packages/aria2/src/index.ts
+++ packages/aria2/src/index.ts
@@ -1,7 +1,13 @@
-import { Aria2, Aria2InstanceConfig } from "@root/packages/aria2/src/aria2";
-import { defaultConfigObj } from "@root/packages/aria2/src/util";
+import { Aria2, Aria2InstanceConfig } from "./aria2";
+import { defaultConfigObj } from "./util";
 
 export const create = (instanceConfig: Aria2InstanceConfig) =>
   new Aria2({ ...defaultConfigObj, ...instanceConfig });
 
 export { Aria2 };
+
+export type {
+  Aria2InstanceConfig,
+  EventSubscribeMap,
+  SocketPendingMap,
+} from "./aria2";
