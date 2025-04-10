14b276ba2140cef42741d441fd04ab02d91bcf6e
Taoister39 <Qiacrkeng@gmail.com>
2025-04-10 19:00:54 +0800

    fix: aria2 default bind

--- packages/aria2/src/index.ts
+++ packages/aria2/src/index.ts
@@ -4,35 +4,35 @@ import { defaultOption } from "./util";
 export const create = (instanceConfig?: Aria2InstanceConfig) =>
   new Aria2({ ...defaultOption, ...instanceConfig });
 
-type DefaultAria2 = {
-  [key in keyof Aria2]: Aria2[key];
-} & {
-  <T>(method: string): Promise<T>;
-};
+// type DefaultAria2 = {
+//   [key in keyof Aria2]: Aria2[key];
+// } & {
+//   <T>(method: string): Promise<T>;
+// };
 
-const instance = create();
+// const instance = create();
 
-const call: DefaultAria2 = (method: string) => {
-  instance.open();
-  return instance.call(method);
-};
+// const call: DefaultAria2 = (method: string) => {
+//   instance.open();
+//   return instance.call(method);
+// };
 
-call.call = instance.call.bind(instance);
-call.open = instance.open.bind(instance);
-call.close = instance.close.bind(instance);
-call.eventSubscribeMap = instance.eventSubscribeMap;
-call.socketPendingMap = instance.socketPendingMap;
-call.fetcher = instance.fetcher;
-call.webSocketIns = instance.webSocketIns;
-call.addListener = instance.addListener.bind(instance);
-call.removeListener = instance.removeListener.bind(instance);
-call.removeAllListener = instance.removeAllListener.bind(instance);
-call.setListener = instance.setListener.bind(instance);
-call.listNotifications = instance.listNotifications.bind(instance);
-call.listMethods = instance.listMethods.bind(instance);
-call.multiCall = instance.multiCall.bind(instance);
+// call.call = instance.call.bind(instance);
+// call.open = instance.open.bind(instance);
+// call.close = instance.close.bind(instance);
+// call.eventSubscribeMap = instance.eventSubscribeMap;
+// call.socketPendingMap = instance.socketPendingMap;
+// call.fetcher = instance.fetcher;
+// call.webSocketIns = instance.webSocketIns;
+// call.addListener = instance.addListener.bind(instance);
+// call.removeListener = instance.removeListener.bind(instance);
+// call.removeAllListener = instance.removeAllListener.bind(instance);
+// call.setListener = instance.setListener.bind(instance);
+// call.listNotifications = instance.listNotifications.bind(instance);
+// call.listMethods = instance.listMethods.bind(instance);
+// call.multiCall = instance.multiCall.bind(instance);
 
-export default call;
+// export default call;
 
 export { Aria2 };
 
