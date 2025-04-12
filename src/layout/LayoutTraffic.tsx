f9c557721ed32c30959647bc5ca1b97993d4b865
Taoister39 <Qiacrkeng@gmail.com>
2025-04-12 20:46:01 +0800

    style: styled for traffic

--- src/layout/LayoutTraffic.tsx
+++ src/layout/LayoutTraffic.tsx
@@ -1,5 +1,5 @@
 import { ArrowDownwardRounded, ArrowUpwardRounded } from "@mui/icons-material";
-import { Box, SxProps, Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import useSWR from "swr";
 
@@ -7,28 +7,29 @@ import { getGlobalStatApi } from "@/services/aria2c_api";
 import { useTaskStore } from "@/store/task";
 import parseByteVo from "@/utils/download";
 
-const boxSx: SxProps = {
+const SpeedIndicatorRow = styled("section")(() => ({
   display: "flex",
   alignItems: "center",
   whiteSpace: "nowrap",
-};
+  "& .MuiSvgIcon-root": {
+    mr: "8px",
+    fontSize: 16,
+  },
+}));
 
-const iconSx: SxProps = {
-  mr: "8px",
-  fontSize: 16,
-};
-const valSx: SxProps = {
+const SpeedValue = styled(Typography)(() => ({
   flex: "1 1 56px",
   userSelect: "none",
   textAlign: "center",
-};
-const unitSx: SxProps = {
+}));
+
+const SpeedUnit = styled(Typography)(() => ({
   flex: "0 1 27px",
   userSelect: "none",
   color: "grey.500",
   fontSize: "12px",
   textAlign: "right",
-};
+}));
 
 function LayoutTraffic() {
   const { t } = useTranslation();
@@ -44,31 +45,28 @@ function LayoutTraffic() {
     { refreshInterval: interval },
   );
 
-  const [up, upUnit] = parseByteVo(Number(stat?.uploadSpeed), "/s");
-  const [down, downUnit] = parseByteVo(Number(stat?.downloadSpeed), "/s");
+  const [up, upUnit] = parseByteVo(stat?.uploadSpeed, "/s");
+  const [down, downUnit] = parseByteVo(stat?.downloadSpeed, "/s");
 
   return (
-    <Box display="flex" flexDirection="column" gap={0.75}>
-      <Box title={t("UploadSpeed")} sx={boxSx}>
-        <ArrowUpwardRounded
-          sx={iconSx}
-          color={+up > 0 ? "secondary" : "disabled"}
-        />
-        <Typography sx={valSx} color="secondary">
-          {up}
-        </Typography>
-        <Typography sx={unitSx}>{upUnit}</Typography>
-      </Box>
-      <Box title={t("DownloadSpeed")} sx={boxSx}>
-        <ArrowDownwardRounded
-          sx={iconSx}
-          color={+down > 0 ? "primary" : "disabled"}
-        />
-        <Typography color="primary" sx={valSx}>
-          {down}
-        </Typography>
-        <Typography sx={unitSx}>{downUnit}</Typography>
-      </Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 0.75,
+      }}
+    >
+      <SpeedIndicatorRow title={t("UploadSpeed")}>
+        <ArrowUpwardRounded color={+up > 0 ? "secondary" : "disabled"} />
+        <SpeedValue color="secondary">{up}</SpeedValue>
+        <SpeedUnit>{upUnit}</SpeedUnit>
+      </SpeedIndicatorRow>
+
+      <SpeedIndicatorRow title={t("DownloadSpeed")}>
+        <ArrowDownwardRounded color={+down > 0 ? "primary" : "disabled"} />
+        <SpeedValue color="primary">{down}</SpeedValue>
+        <SpeedUnit>{downUnit}</SpeedUnit>
+      </SpeedIndicatorRow>
     </Box>
   );
 }
