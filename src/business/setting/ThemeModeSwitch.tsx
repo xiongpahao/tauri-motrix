3a459da75a16b55f8d25640a11d5bbaefcb8c336
Taoister39 <Qiacrkeng@gmail.com>
2025-04-12 20:47:46 +0800

    chore: theme

--- src/business/setting/ThemeModeSwitch.tsx
+++ src/business/setting/ThemeModeSwitch.tsx
@@ -1,7 +1,7 @@
 import { Button, ButtonGroup, useColorScheme } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
-import { THEME_MODES } from "@/constant/theme";
+export const THEME_MODES = ["light", "dark", "system"] as const;
 
 function ThemeModeSwitch() {
   const { t } = useTranslation();
--- src/constant/theme.ts
+++
@@ -1,3 +0,0 @@
-export const THEME_MODES = ["light", "dark", "system"] as const;
-
-export type ThemeMode = "light" | "dark" | "system";
