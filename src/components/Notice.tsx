a9ca50188b3f69c110aae261d15af88b08b66c9b
Taoister39 <Qiacrkeng@gmail.com>
2025-04-12 21:37:29 +0800

    feat: notice component

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,17 +1,95 @@
-import { ReactNode } from "react";
+import {
+  CheckCircleRounded,
+  CloseRounded,
+  ErrorRounded,
+} from "@mui/icons-material";
+import {
+  Box,
+  IconButton,
+  Slide,
+  Snackbar,
+  SnackbarProps,
+  styled,
+  Typography,
+} from "@mui/material";
+import { ReactNode, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 interface InnerProps {
-  type: string;
+  isDark?: boolean;
+  type: keyof NoticeInstance;
   duration?: number;
   message: ReactNode;
-  isDark?: boolean;
   onClose: () => void;
 }
 
-const NoticeInner = (props: InnerProps) => {
-  return <></>;
-};
+const TextInner = styled(Typography)(() => ({
+  wordWrap: "break-word",
+  width: "calc(100% - 35px)",
+}));
+
+const MessageInner = styled(Box)(() => ({
+  width: 328,
+  display: "flex",
+  alignItems: "center",
+  gap: 2,
+}));
+
+function NoticeInner({ onClose, message, duration, type, isDark }: InnerProps) {
+  const [visible, setVisible] = useState(true);
+
+  const onBtnClose = () => {
+    setVisible(false);
+    onClose();
+  };
+
+  const onAutoClose: SnackbarProps["onClose"] = (_, reason) => {
+    if (reason !== "clickaway") {
+      onBtnClose();
+    }
+  };
+
+  const msgElement =
+    type === "info" ? (
+      message
+    ) : (
+      <MessageInner>
+        {type === "error" ? (
+          <ErrorRounded color="error" />
+        ) : (
+          <CheckCircleRounded color="success" />
+        )}
+
+        <TextInner>{message}</TextInner>
+      </MessageInner>
+    );
+
+  return (
+    <Snackbar
+      open={visible}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      autoHideDuration={duration === -1 ? null : duration || 1500}
+      onClose={onAutoClose}
+      message={msgElement}
+      sx={{
+        maxWidth: 360,
+        ".MuiSnackbarContent-root": {
+          bgcolor: isDark ? "#50515C" : "#ffffff",
+          color: isDark ? "#ffffff" : "#000000",
+        },
+      }}
+      transitionDuration={200}
+      action={
+        <IconButton size="small" color="inherit" onClick={onBtnClose}>
+          <CloseRounded fontSize="inherit" />
+        </IconButton>
+      }
+      slots={{
+        transition: (p) => <Slide {...p} direction="left" />,
+      }}
+    />
+  );
+}
 
 interface NoticeInstance {
   (props: Omit<InnerProps, "onClose">): void;
