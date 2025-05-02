aeb99cd0201a28df2c97fc3f04502c6b155d1922
Taoister39 <Qiacrkeng@gmail.com>
2025-05-02 22:53:17 +0800

    test: no mock tauri api

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,4 +1,13 @@
 import "@testing-library/jest-dom";
 
+import { randomFillSync } from "crypto";
 import { TextDecoder, TextEncoder } from "util";
 Object.assign(global, { TextEncoder, TextDecoder });
+
+Object.defineProperty(window, "crypto", {
+  value: {
+    getRandomValues: (buffer) => {
+      return randomFillSync(buffer);
+    },
+  },
+});
--- jest.config.ts
+++ jest.config.ts
@@ -21,7 +21,6 @@ export default {
     ],
   },
   moduleNameMapper: {
-    "^@tauri-apps/api/core$": "<rootDir>/tests/__mocks__/tauri_api.js",
     "\\.svg\\?react$": "<rootDir>/tests/__mocks__/svgr_mock.js",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/__mocks__/file_mock.js",
