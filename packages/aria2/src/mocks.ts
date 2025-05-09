b79b0450fd4d2e0e4ac193c1c6b3cb96b40e2713
Taoister39 <Qiacrkeng@gmail.com>
2025-05-10 03:17:51 +0800

    feat(aria2): mockRPC & clearMocks

---
+++ packages/aria2/src/mocks.ts
@@ -0,0 +1,32 @@
+import { CallParam, JsonRpcRequest } from "./util";
+
+let originalFetch: typeof globalThis.fetch | null;
+
+export function mockRPC(cb: (method: string, payload: unknown) => unknown) {
+  originalFetch = globalThis.fetch;
+  // @ts-expect-error only mock on unit test
+  globalThis.fetch = (_, option: { body: string }) => {
+    const data: JsonRpcRequest = JSON.parse(option.body);
+
+    let result;
+    if (data.method === "system.multicall") {
+      const results = (
+        data.params[0] as Array<{ methodName: string; params: CallParam }>
+      ).map(({ methodName, params }) => [cb(methodName, params)]);
+      result = results;
+    } else {
+      result = cb(data.method, data.params);
+    }
+
+    return Promise.resolve({
+      json: () => Promise.resolve({ result }),
+    });
+  };
+}
+
+export function clearMocks() {
+  if (originalFetch) {
+    globalThis.fetch = originalFetch;
+    originalFetch = null;
+  }
+}
